f0c3bc6cbb38273bf1ba4cc4a014b97ea2f0c98f
kris <kristina.chitaia1@gmail.com>
2024-01-27 22:42:51 +0400

    ADD: Carousel && FAQ

--- ChangeLog.md
+++ ChangeLog.md
@@ -19,10 +19,23 @@ All notable changes to this project will be documented in this file.
 - SCSS structure
 - main SCSS
 - fonts
-- JSON server
 - parcel package for static files
 
 ### FIXED:
 
 - package.json
 - faq section
+
+## [0.2.0] - 23.01.2024
+
+### ADDED:
+
+- navigation
+- animations
+- create HamburgerMenu class
+- create Carousel class
+- create Questions class
+
+### FIXED:
+
+- styles
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,16 @@
-// const questionBtn = document.querySelectorAll('.faq-question');
+import Carousel from './carousel.min';
+import Questions from './question.min';
+import HamburgerMenu from './humburgerMenu.min';
 
-// questionBtn.forEach((question, index) => {
-//   //   console.log(question);
-//   question.addEventListener('click', function () {
-//     this.nextElementSibling.classList.toggle('hidden');
-//   });
-// });
+const url =
+  'https://api.jsonsilo.com/public/d2101e93-3f41-48fd-85eb-9a7abbb79b2e';
+
+const hamburgerMenu = new HamburgerMenu();
+const carousel = new Carousel('.carousel-container', {
+  interval: 2000,
+});
+const faqSection = new Questions('.faq-questions-wrapper', url, 3);
+
+hamburgerMenu.init();
+carousel.init();
+faqSection.init();
